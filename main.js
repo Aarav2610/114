a80cef3b263266eb653f38fc47cccbfb84055aac
Aarav2610 <89948663+Aarav2610@users.noreply.github.com>
2021-11-13 17:34:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    }
+    
+    function setup() {
+      createCanvas(300, 300);
+      canvas.center();
+    }
+    
+    function draw() {
+    }
+    
+
+    function take_snapshot(){
+       save('myFilterImage.png');
+    }
+    
